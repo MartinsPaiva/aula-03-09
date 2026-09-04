511a2227423a17b67223811a818054efb292193c
MartinsPaiva <martinspaiva99@gmail.com>
2026-09-03 22:24:51 -0300

    Add files via upload
    html css e js

---
+++ Aula Js 0309/js/ex1.js
@@ -0,0 +1,6 @@
+//variáveis
+var nome = prompt("Digite seu nome: ");
+
+//Comando escrever
+console.log("Seu nome é: ", nome);
+alert("Seu nome é: "+ nome);
---
+++ Aula Js 0309/js/ex2.js
@@ -0,0 +1,7 @@
+//Entrada de dados
+var x = parseFloat(prompt("Digite o valor de x: "));
+var x = parseFloat(prompt("Digite o valor de y: "));
+//processament
+var soma = x + y;
+//saída de dados
+console.log("Valor da soma é: ", soma);
